f82436941955e4c664fbb5871f1592e18187e34b
João <jmcrocha333@gmail.com>
2023-12-15 13:52:34 +0000

    .

--- nbaProject/src/views/Games.vue
+++ nbaProject/src/views/Games.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <!-- mostrar uma lista de jogos com data, home_team.abbreviation and visitor_team.abbreviation -->
-        <table class="table table-striped" border="1">
+        <table class="table table-striped">
             <thead>
                 <tr>
                     <th scope="col">Data</th>
-                    <th scope="col">Time da casa</th>
-                    <th scope="col">Time visitante</th>
+                    <th scope="col">Equipa da casa</th>
+                    <th scope="col">Equipa visitante</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="game in nbaStore.getGames" :key="game.id">
                     <td>{{ game.date }}</td>
-                    <td>{{ game.home_team.abbreviation }}-{{game.home_team_score}}</td>
-                    <td>{{ game.visitor_team.abbreviation }}-{{game.visitor_team_score}}</td>
+                    <td>{{ game.home_team.abbreviation }} - {{ game.home_team_score }}</td>
+                    <td>{{ game.visitor_team.abbreviation }} - {{ game.visitor_team_score }}</td>
                 </tr>
             </tbody>
         </table>
@@ -39,6 +39,28 @@ import { useNBAStore } from '../stores/counter.js'
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+  /* Add your custom styles here if needed */
+  .table {
+    width: 100%;
+    margin-bottom: 1rem;
+    color: white; /* Use a darker color for better contrast */
+  }
 
+  th, td {
+    padding: 0.75rem;
+    text-align: left;
+    border-bottom: 1px solid #dee2e6;
+    /* bordas redondas */
+    border-radius: 3px;
+  }
+
+  th {
+    background-color: grey; /* Use a softer background color */
+    font-weight: bold;
+  }
+
+  tbody tr:hover {
+    background-color: burlywood;
+  }
 </style>
