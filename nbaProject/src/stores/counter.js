0b12bd523e62cb20cb18456c25df13b286619205
João <jmcrocha333@gmail.com>
2023-12-15 13:29:17 +0000

    .

--- nbaProject/src/stores/counter.js
+++ nbaProject/src/stores/counter.js
@@ -1,12 +1,36 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import * as api from '../api/api.js'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+const NBA_API_URL = 'https://www.balldontlie.io/api/v1'
+
+export const useNBAStore = defineStore('nbastore', {
+  state: () => ({
+    players:[],
+    player:{},
+  }),
+  getters: {
+    getPlayers: (state) => state.players,
+    getPlayer: (state) => state.player
+  },
+  actions:{
+    async fetchPlayers() {
+      try {
+        const players = await api.get(NBA_API_URL, 'players')
+        this.players = players.data
+      } catch (error) {
+        console.error(`Error fetching players:`, error)
+        throw error
+      }
+    },
+    async fetchPlayer(id) {
+      try {
+        const player = await api.get(NBA_API_URL, `players/${id}`)
+        this.player = player
+      } catch (error) {
+        console.error(`Error fetching player:`, error)
+        throw error
+      }
+    },
   }
-
-  return { count, doubleCount, increment }
 })
--- nbaProject/src/views/HomeView.vue
+++ nbaProject/src/views/HomeView.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-
+  <div class="nba-players-container">
+    <h1 class="text-center mb-4">NBA Players</h1>
+    <div class="container">
+      <div class="row">
+        <div v-for="player in nbaStore.getPlayers" :key="player.id" class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <div class="card-body">
+              <h3 class="card-title">{{ player.first_name }} {{ player.last_name }}</h3>
+              <p class="card-text"><strong>Position:</strong> {{ player.position }}</p>
+              <p class="card-text"><strong>Team:</strong> {{ player.team.full_name }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { useNBAStore } from '../stores/counter.js'
   export default {
-    
+    data() {
+      return {
+        nbaStore: useNBAStore(),
+        players: [],
+      }
+    },
+    created() {
+        try {
+            this.nbaStore.fetchPlayers();
+        } catch (error) {
+            alert(error.message);
+        }
+    },
   }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.nba-players-container {
+  padding: 20px;
+}
 
+.card {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card-title {
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+}
+
+.card-text {
+  margin-bottom: 0.3rem;
+}
 </style>
