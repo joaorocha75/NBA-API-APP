453726e01ef73ef33489076ba8c509eb5a1a678e
Junqingchen01 <92992695+Junqingchen01@users.noreply.github.com>
2023-12-22 19:44:28 +0000

    haeder css

--- nbaProject/src/components/Header.vue
+++ nbaProject/src/components/Header.vue
@@ -3,13 +3,13 @@
     <div class="nba-icon">
       <img src="https://seeklogo.com/images/N/nba-logo-41668C66DB-seeklogo.com.png" alt="NBA Icon" />
     </div>
-    <div class="nav-item">
+    <div class="nav-item" :class="{ 'active': $route.path === '/' }">
       <router-link to="/">Games</router-link>
     </div>
-    <div class="nav-item">
+    <div class="nav-item" :class="{ 'active': $route.path === '/news' }">
       <router-link to="/news">News</router-link>
     </div>
-    <div class="nav-item">
+    <div class="nav-item" :class="{ 'active': $route.path === '/teams' }">
       <router-link to="/teams">Teams</router-link>
     </div>
   </div>
@@ -36,20 +36,32 @@ export default {
   padding: 10px;
   display: flex;
   justify-content: space-around;
-  align-items: center; /* Center items vertically */
+  align-items: center; 
   z-index: 1000;
 }
 
 .nba-icon img {
-  width: 70px; /* Increase the width as needed */
+  width: 70px; 
   height: auto;
   display: block;
   margin-right: 10px;
-  margin: 0; /* Remove any default margin */
+  margin: 0; 
 }
 
 .nav-item {
   margin-right: 20px;
+  position: relative;
+}
+
+.nav-item.active::after {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: 1;
+  left: 0;
+  width: 100%;
+  height: 5px;
+  background-color: red; 
 }
 
 a {
